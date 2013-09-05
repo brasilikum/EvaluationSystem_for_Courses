ab75f7e00454dde6e3a7f82827213325c863d67b
Brasilikum <mail@georg-hartmann.com>
2013-09-05 18:23:24 +0200

    _initAuth deleted for Master-Branch

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -25,12 +25,12 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     }
 
               
-    protected function _initAuth()
+    /*protected function _initAuth()
     {
         $this->bootstrap('frontController');
         $auth = Zend_Auth::getInstance();
         $acl = new Application_Plugin_Auth_Acl();
         $this->getResource('frontController')->registerPlugin(new Application_Plugin_Auth_AccessControl($auth, $acl))->setParam('auth', $auth);
-    }
+    }*/
 }
 
