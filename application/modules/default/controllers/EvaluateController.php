95a657cf85b2d510954658df584433119aae4352
Brasilikum <mail@georg-hartmann.com>
2013-09-05 15:42:59 +0200

    Form experiments

--- application/modules/default/controllers/EvaluateController.php
+++ application/modules/default/controllers/EvaluateController.php
@@ -23,20 +23,48 @@ class EvaluateController extends Zend_Controller_Action
 
 	}
 
-	public function evaluateAction(){
+	public function evaluateAction()
+	{
+		$form = $this->generateForm();
 		$evalID = null;
 		$request = $this->getRequest();
 
 		if ($request->isGet()){
-			$evalID = (int)$request->getParam('evalid');
+			$evalID = (int)$request->getParam('id');
 		}
 
 		echo $evalID;
+
+		$this->view->form = $form;
 
 		//zu evalID die entsprechende Evaluation suchen
 		//entspechendes Formular aufbauen
 	}
 
+	private function generateForm()
+	{
+		$form = new Zend_Form;
+		$form->setMethod('post');
+		$nameElement = new Zend_Form_Element_Text(
+		'name', array('label' => 'Name'));
+		$nameElement->setRequired(true);
+		$nameElement->addFilter('StripTags');
+
+		$radioElement = new Zend_Form_Element_Radio(
+		'radio', array('label' => 'Radio'));
+		$radioElement->setMultiOptions(array(0 => 0, 1));
+
+		$submitElement = new Zend_Form_Element_Submit(
+		'submit', array('label'=>'Speichern'));
+
+		$elements = array($nameElement, 
+							$radioElement, 
+							$submitElement);
+
+		$form->addElements($elements);
+
+		return $form;
+	}
 
 }
 
