4b85b1c615cb4362a8d211de74b46a9e5d712fed
PNardman <philipp.nardmann@hs-osnabrueck.de>
2013-09-05 16:09:15 +0200

    Admin controller hinzugefügt
    Zum testen der ACL

---
+++ application/modules/admin/controllers/IndexController.php
@@ -0,0 +1,44 @@
+<?php
+
+class Admin_IndexController extends Zend_Controller_Action
+{
+
+	protected $answerToQuestionTable;
+	protected $questionTable;
+	protected $questionnaireTable;
+
+
+	public function init()
+	{
+		$this->answerToQuestionTable = new Application_Model_DbTable_AnswerToQuestionTable();
+		$this->questionnaireTable = new Application_Model_DbTable_QuestionnaireTable();
+		$this->questionTable = new Application_Model_DbTable_QuestionTable();
+		
+			
+	}
+
+	public function indexAction()
+	{
+
+			$this->view->form = $this->_getLoginForm();
+
+			
+
+	}
+
+	public function _getLoginForm(){
+
+	    $form = new Zend_Form();
+		$form->setAction('')->setMethod('post');
+		$user = new Zend_Form_Element_Text('username', array('label' => 'Benutzername', 'required' => true));
+		$password = new Zend_Form_Element_Password('password', array('label' => 'Passwort', 'required' => true));
+		$submit = new Zend_Form_Element_Submit('submit', array('label' => 'Anmelden'));
+		$form->addElements(array($user, $password, $submit));
+
+		return $form;
+
+	}
+
+
+}
+
